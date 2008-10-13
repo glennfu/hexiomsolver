b11d61c187caeecf2c30bc74a0e3df21cac12ec0
Glenn Ford <glenn@glennfu.com>
2008-10-13 19:18:03 -0400

    Added -v for Verbose to print the before/after

--- hexiom.rb
+++ hexiom.rb
@@ -4,6 +4,7 @@ require File.dirname(__FILE__) + "/bencher"
 
 LEVEL = $*[$*.index("-l")+1].to_i rescue 2
 USE_TREE = ($*[$*.index("-t")+1] == 'true' ? true : false) rescue false
+VERBOSE = ($*[$*.index("-v")+1] == 'true' ? true : false) rescue false
 
 puts " Level #{LEVEL}, " + (USE_TREE ? "Using Tree" : "Using Hash")
 
@@ -11,8 +12,10 @@ NO_SPACE = 9
 BLANK_SPACE = 8
 
 board = BoardLoader.load(LEVEL) #11)
-board.print
-puts ""
+if VERBOSE
+  board.print
+  puts ""
+end
 
 valid_pieces = board.flatten.reject{|x| (x == NO_SPACE || x < 0 || x == BLANK_SPACE) }.sort.reverse
 # puts "valid pieces: #{valid_pieces.inspect}"
@@ -22,7 +25,7 @@ solver = HexiomSolver.new(:tree => USE_TREE)
 answer = solver.find_solution(BoardLoader.empty_board, valid_pieces)
 if answer
   puts " Solution found"
-  answer.print
+  answer.print if VERBOSE
 else
   puts " No solution found"
 end
